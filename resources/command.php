f90f5600ff2ee54f7bb4888c06c188b89761922e
BlackZeroPM <pocketminevnp@gmail.com>
2017-05-06 17:09:56 +0700

    Create command.php

---
+++ resources/command.php
@@ -0,0 +1,18 @@
+setpoint:
+- setpoint
+seepoint:
+- seepoint
+mypoint:
+- mypoint
+pay:
+- pay
+givepoint:
+- givepoint
+toppoint:
+- toppoint
+setlang:
+- setlang
+takepoint:
+- takepoint
+mystatus:
+- mystatus
